acd8755f54547b282a5b48c78b20dfca6eede3a8
Sawwaty <yezhovye@mac.com>
2021-07-06 12:10:52 +0300

    Initial Commit

---
+++ AnimalsHotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalsHotel
+//
+//  Created by Victoria Klimova on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
